d930f98d8bfa5b7d892d5e3b17a96968c58074d6
jotavasquez <jotavasquezjota@gmail.com>
2017-11-07 21:07:04 -0300

    v0.02

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 
-var boton = document.getElementById("nameList");
+var boton = document.getElementById("btn");
 
+//despliega el menu para el nombre de la lista
 function showMenu() {
     var menu = document.getElementById('nameInput');
     
@@ -8,9 +9,13 @@ function showMenu() {
         menu.classList.remove("disable-menu");
         menu.classList.add("enable-menu");
         document.getElementById("nameList").setAttribute("id", "nameListWhite");
-        console.log("mostrar");
+        boton.classList.add("disable-menu");
+        console.log("muestra");
+        var titleForm = document.getElementById("form")[0];
+        console.log(titleForm);
+
     } else {
-        console.log("ocultar");
+        console.log("oculta");
         document.getElementById("nameListWhite").setAttribute("id", "nameList");
         menu.classList.remove("enable-menu");
         menu.classList.add("disable-menu");
@@ -18,3 +23,46 @@ function showMenu() {
 }
 
 boton.addEventListener("click", showMenu);
+
+//guarda el nombre de la lista
+var savebtn = document.getElementById("save");
+
+function saveListName() {
+
+    //tenemos que tomar el texto ingresado en el input
+    var name = document.getElementById('nameListWhite').value;
+        console.log(name);
+
+    //limpiar el input
+     document.getElementById('nameListWhite').value = '';
+     //contenedor que esta en el html
+    var cont = document.getElementById('form');
+        console.log(cont);
+
+    //creamos el div que contiene el titulo
+    var titleDiv = document.createElement('div');
+        console.log(titleDiv);
+
+    //nodos de texto del textarea
+    var textTitle = document.createTextNode(name);
+            console.log(textTitle);
+
+    //agregar el texto
+    var containerElementP = document.createElement('p');
+    console.log(containerElementP);
+    containerElementP.appendChild(textTitle);
+    titleDiv.appendChild(containerElementP);
+    cont.appendChild(titleDiv);
+
+    // llevar nodo arriba, como primer hijo arriba del input
+    //variable nodo padre
+    var parent = document.getElementById("nameInput");
+    // variable nodo de referencia input
+    var nameList= document.getElementById("nameList");
+    //variable con la posición a colocar
+    var insertName = parent.insertBefore(containerElementP, nameList);
+    console.log(insertName);
+}
+
+// correr funcion
+savebtn.addEventListener("click", saveListName);
